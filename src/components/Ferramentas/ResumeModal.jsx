9a00f62964562d9b99a3bdd630a40050c2264cdd
Thomas Eduardo <developer.thomas@outlook.com.br>
2025-01-11 05:15:44 -0300

    ajustndo

--- src/components/Ferramentas/ResumeModal.jsx
+++ src/components/Ferramentas/ResumeModal.jsx
@@ -38,7 +38,7 @@ const ResumeModal = () => {
       {/* Modal */}
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-          <div className="bg-white rounded-lg p-6 w-11/12 max-w-2xl max-h-[80vh] overflow-y-auto">
+          <div className="bg-whiblackte rounded-lg p-6 w-11/12 max-w-2xl max-h-[80vh] overflow-y-auto">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-xl font-bold">Currículo de Thomas Eduardo</h2>
               <button onClick={closeModal} className="text-gray-500 hover:text-gray-800">
